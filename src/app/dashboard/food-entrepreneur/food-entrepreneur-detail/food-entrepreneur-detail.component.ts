c984a01f1c1fd8efb90a3059f51097b0023856cb
Andrew <andrew@dutchfrontiers.com>
2017-08-22 16:56:08 +0200

    added food entrepreneur overview

--- src/app/dashboard/food-entrepreneur/food-entrepreneur-detail/food-entrepreneur-detail.component.ts
+++ src/app/dashboard/food-entrepreneur/food-entrepreneur-detail/food-entrepreneur-detail.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {HttpService} from '../../../services/http.service';
 
 @Component({
-  selector: 'app-food-entrepreneur-detail',
-  templateUrl: './food-entrepreneur-detail.component.html',
-  styleUrls: ['./food-entrepreneur-detail.component.scss']
+    selector: 'app-food-entrepreneur-detail',
+    templateUrl: './food-entrepreneur-detail.component.html',
+    styleUrls: ['./food-entrepreneur-detail.component.scss']
 })
 export class FoodEntrepreneurDetailComponent implements OnInit {
 
-  constructor() { }
+    id;
+    foodEntrepreneur: any = {};
 
-  ngOnInit() {
-  }
+    constructor(private httpService: HttpService,
+                private activatedRoute: ActivatedRoute,
+                private router: Router) {
+    }
+
+    async ngOnInit() {
+        this.activatedRoute.params.subscribe(params => {
+            this.id = params['id'];
+        });
+
+        if (this.id !== 'new') {
+            try {
+                this.foodEntrepreneur = await this.httpService.getData(`/foodEntrepreneurs/${this.id}`);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    }
+
+    async save(foodEntrepreneur) {
+        try {
+            if (this.id === 'new') {
+                await this.httpService.postData('/foodEntrepreneurs', {foodEntrepreneur});
+            } else {
+                await this.httpService.patchData('/foodEntrepreneurs', {foodEntrepreneur});
+            }
+
+            this.router.navigate(['/dashboard/food-entrepreneurs']);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async delete() {
+        try {
+            await this.httpService.deleteData(`/foodEntrepreneurs/${this.id}`);
+            this.router.navigate(['/dashboard/food-entrepreneurs']);
+        } catch (err) {
+            console.log(err);
+        }
+    }
 
 }
--- src/app/dashboard/food-entrepreneur/food-entrepreneur-overview/food-entrepreneur-overview.component.ts
+++ src/app/dashboard/food-entrepreneur/food-entrepreneur-overview/food-entrepreneur-overview.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpService} from '../../../services/http.service';
 
 @Component({
-  selector: 'app-food-entrepreneur-overview',
-  templateUrl: './food-entrepreneur-overview.component.html',
-  styleUrls: ['./food-entrepreneur-overview.component.scss']
+    selector: 'app-food-entrepreneur-overview',
+    templateUrl: './food-entrepreneur-overview.component.html',
+    styleUrls: ['./food-entrepreneur-overview.component.scss']
 })
 export class FoodEntrepreneurOverviewComponent implements OnInit {
 
-  constructor() { }
+    foodEntrepreneurs = [];
 
-  ngOnInit() {
-  }
+    constructor(private httpServer: HttpService) {
+    }
 
+    async ngOnInit() {
+        try {
+            this.foodEntrepreneurs = await this.httpServer.getData('/foodEntrepreneurs');
+        } catch (err) {
+            console.log(err);
+        }
+    }
 }
